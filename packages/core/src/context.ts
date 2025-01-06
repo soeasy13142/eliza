c4b046a7d51fccc194c0f09c2a076892f5dbb783
Sayo <82053242+wtfsayo@users.noreply.github.com>
2025-01-06 19:26:44 +0530

    Revert "add santisation for handlebar template"
    This reverts commit 3a03592293981032315d4045ab950e6d083912fb.

--- packages/core/src/context.ts
+++ packages/core/src/context.ts
@@ -35,12 +35,6 @@ export const composeContext = ({
     template: string;
     templatingEngine?: "handlebars";
 }) => {
-
-    // Sanitize the template by only allowing specific patterns
-    if (!/^[^{]*{{[\s\w]+}}[^}]*$/.test(template)) {
-        throw new Error("Invalid template format. Only simple variable substitutions are allowed.");
-    }
-
     if (templatingEngine === "handlebars") {
         const templateFunction = handlebars.compile(template);
         return templateFunction(state);
